6cbd1e3d11bdd338cf28fb712433e92d5289c0ac
yonn28 <ycnovac@unal.edu.co>
2021-04-25 09:07:22 -0500

    adding module resolution node

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-import { of, Subject } from "../node_modules/rxjs/index";
+import { combineLatest, fromEvent, of } from "rxjs"
+import { pluck, startWith } from "rxjs/operators"
 
 
-of(1,2,3).subscribe(console.log)
+
+const gameSize = 1000;
+const creategameObject = ( x: any ,y: any ) => ({ x, y })
+
+
+combineLatest(
+    of({ ...creategameObject(gameSize/2 , gameSize/2 -1) , score: 0, lives: 3 }),
+    fromEvent(document, 'keyup').pipe(startWith(''),pluck('code'))
+)
+
+
